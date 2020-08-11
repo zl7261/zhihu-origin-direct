3d00a0730fb6d173fc733bbc7fb162d03b6b1347
zl7261 <zhenliu7261@gmail.com>
2020-08-11 22:28:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         去除知乎安全中心。
+// @name         知乎|简书-直接跳转
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      2.0
 // @description  屏蔽知乎安全中心，直接跳转链接地址。
 // @author       zl7261
 // @match        https://*.zhihu.com/*
+// @match        https://*.jianshu.com/*
 // @grant        none
 // @license      GPLv3
 // ==/UserScript==
@@ -12,18 +13,60 @@
 // forked from https://greasyfork.org/zh-CN/scripts/396857
 // changelog:
 // 2020-08-06：  improve performance
-//               更新domApi 直接查询有href属性的a标签 使用observerApi而非监听scroll事件
+//               直接查询有href属性的a标签 使用observerApi而非监听scroll事件
+// 2020-08-06：  code format
+//               支持简书 更新 dom selector
 
 (function () {
     'use strict';
 
+    const url = window.location.href
+    if (!url.match(/\.zhihu\.|\.jianshu\./)) {
+        return
+    }
 
-    /// 地址类型
-    /// https://link.zhihu.com/?target=http%3A//www.test.com
+    let zhihuFlag = url.indexOf('.zhihu.') > -1
 
-    /// 获取所以a标签
-    /// 循环判断 a 标签是否包含两个 http 字样
-    /// 截取最后一个 http 内容
+
+    const getPrefix = (isZhihu) => {
+        if (isZhihu) {
+            return "link.zhihu.com"
+        } else {
+            return "link.jianshu.com"
+        }
+    }
+
+    const getSplit = (isZhihu) => {
+        if (isZhihu) {
+            return "target="
+        } else {
+            return "/?t="
+        }
+    }
+    const getSelector = (isZhihu) => {
+        if (isZhihu) {
+            return ".ListShortcut a[href]"
+        } else {
+            return "[role = main] a[href]"
+        }
+    }
+
+    const prefix = getPrefix(zhihuFlag)
+    const split = getSplit(zhihuFlag)
+    const selector = getSelector(zhihuFlag)
+
+    // 获取正确的地址用于跳转
+    const getOriginHref = () => {
+        let documents = document.querySelectorAll(selector)
+        for (let i = 0; i < documents.length; i++) {
+            if (documents[i].href.indexOf(prefix) === -1) {
+                continue;
+            }
+
+            documents[i].setAttribute("href", decodeURIComponent(documents[i].href.split(split)[1]))
+
+        }
+    }
 
     const observer = new ResizeObserver(entries => {
         for (const entry of entries) {
@@ -33,17 +76,4 @@
     })
 
     observer.observe(document.querySelector('body'))
-
-
-    /// 获取正确的地址用于跳转
-    function getOriginHref() {
-        var zhihuPrefix = "link.zhihu.com"
-        var documents = document.querySelectorAll('.ListShortcut a[href]');
-        for (var i = 0; i < documents.length; i++) {
-            if (documents[i].href.indexOf(zhihuPrefix) != -1) {
-                documents[i].setAttribute("href", decodeURIComponent(documents[i].href.split("target=")[1]))
-            }
-        }
-    }
-
 })();
